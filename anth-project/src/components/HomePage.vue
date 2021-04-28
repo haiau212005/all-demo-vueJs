bd9b2ba22b0734f7eaaca57b9bcff779c98a5913
haiau212005 <haiau212005@githud.com>
2021-04-28 11:22:53 +0700

    ok men

---
+++ anth-project/src/components/HomePage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      <h2>Home Page</h2>
+      <table>
+          <tr>
+              <th>id</th>
+              <th>name</th>
+          </tr>
+          <tr v-for="item in sData" v-bind:key="item.id">
+              <td>{{ item.id }}</td>
+              <td>{{ item.name }}</td>
+          </tr>
+      </table>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            sData: this.$sData
+        }
+    },
+    mounted () {
+        //console.log();
+    },
+}
+</script>
+
+<style>
+
+</style>
