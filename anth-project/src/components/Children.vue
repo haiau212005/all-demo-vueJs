ad399cc3a8c8b529f777dd7260df489bd94d5502
haiau212005 <haiau212005@githud.com>
2021-04-29 14:06:31 +0700

    emit ne

---
+++ anth-project/src/components/Children.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <p>day là children</p>
+      <button type="button" v-on:click="PassData">send</button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        PassData() {
+            this.$emit('PassNow', 100)
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
